9ebea9062fc32568d50e02e102604445b2de7b2e
Saman <saman_mehr_ali_poor@tutanota.com>
2024-03-19 21:57:12 +0330

    multiple return added

--- src/App.jsx
+++ src/App.jsx
@@ -8,39 +8,55 @@ function App() {
   // const [count,setValue] = useState(0);
   const [users,setUsers] = useState([]);
   const [urlPage,setUrl] = useState([]);
+  const [isLoaded,setLoad] = useState(false);
+
   document.title = "users";
   useEffect(()=>{
     let req = new Request ("https://api.github.com/users",{ method: "GET"});
     const loadData = async ()=>{
-      const result = await fetch(req).then((res)=>{return res.json();});
-      console.log("+++");
+      const result = await fetch(req).
+      then(res =>{
+        if (!res.ok)
+         throw new Error(res.statusText);
+        else{
+          setLoad(true);
+          return res.json();
+        }
+      });      
       setUsers(result);
       }
     loadData();
   },[]);
-  
-  return (
-    <>
-      <div className='all-usr'>
-      {users.map((usr,index)=>{
-        return <div className='user-info' key={index}>
-
-          <div className='img-container'>
-            <img className='image' src={usr.avatar_url} alt="avatar" />
-          </div>
-
-          <div className='username-container'>
-            <p className='username'>{usr.login}
-            </p>
 
 
-            <p className='link'><a href={usr.html_url} target='_blank'>see page</a></p>
-          </div>
-          </div> ;} 
-        )}
-      </div>
-    </>
-  )
+  if (!isLoaded){
+    return <h1>loading ... </h1>
+  }
+  else
+    return (
+      <>
+
+        <p className='list-title'>top github users:</p>
+        <div className='all-usr'>
+        {users.map((usr,index)=>{
+          return <div className='user-info' key={index}>
+
+            <div className='img-container'>
+              <img className='image' src={usr.avatar_url} alt="avatar" />
+            </div>
+
+            <div className='username-container'>
+              <p className='username'>{usr.login}
+              </p>
+
+
+              <p className='link'><a href={usr.html_url} target='_blank'>see page</a></p>
+            </div>
+            </div> ;} 
+          )}
+        </div>
+      </>
+    )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
+document.title = "react pro";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
    <App/>
