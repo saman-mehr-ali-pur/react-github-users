958c54c7abc85b02387500d26347393d82162e25
Saman <saman_mehr_ali_poor@tutanota.com>
2024-03-27 21:49:34 +0330

    useing custom hook for loading data

--- src/App.jsx
+++ src/App.jsx
@@ -2,44 +2,45 @@ import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import fetchData from './customHook/fetchData'
 function App() {
 
   // const [count,setValue] = useState(0);
-  const [users,setUsers] = useState([]);
-  const [urlPage,setUrl] = useState([]);
-  const [isLoaded,setLoad] = useState(false);
-
+  const [loaded,isError,users] = fetchData("https://api.github.com/users")
   document.title = "users";
-  useEffect(()=>{
-    let control = new AbortController();
-    let signal = console.signal;
-    let req = new Request ("https://api.github.com/users",{ method: "GET"});
-    const loadData = async ()=>{
-      const result = await fetch(req,{signal:signal}).
-      then(res =>{
-        if (!res.ok)
-         throw new Error(res.statusText);
-        else{
-          setLoad(true);
-          return res.json();
-        }
-      });      
-      setUsers(result);
-      }
-    loadData();
+  // useEffect(()=>{
+  //   let control = new AbortController();
+  //   let signal = console.signal;
+  //   let req = new Request ("https://api.github.com/users",{ method: "GET"});
+  //   const loadData = async ()=>{
+  //     const result = await fetch(req,{signal:signal}).
+  //     then(res =>{
+  //       if (!res.ok)
+  //        throw new Error(res.statusText);
+  //       else{
+  //         setLoad(true);
+  //         return res.json();
+  //       }
+  //     });      
+  //     setUsers(result);
+  //     }
+  //   loadData();
 
 
-    return () =>{
-          control.abort();
-    }
-  },[]);
+  //   return () =>{
+  //         control.abort();
+  //   }
+  // },[]);
 
 
-  if (!isLoaded){
+  if (!loaded){
+    console.log("if :"+(users==null))
     return <h1>loading ... </h1>
   }
-  else
+  // if(isError)
+  //   return <h1>{isError}</h1>
+  else{
+    console.log("else: "+(users==null));
     return (
       <>
 
@@ -64,6 +65,7 @@ function App() {
         </div>
       </>
     )
+        }
 }
 
 export default App
